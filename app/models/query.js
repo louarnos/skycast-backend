800d3c86036ab0c8cbc2e863a0c6df7cee46cdde
louarnos <louarnos@gmail.com>
2016-07-09 21:10:22 -0400

    Current Forecast Query Post works
    YAY need to do historical now.

--- app/models/query.js
+++ app/models/query.js
@@ -4,94 +4,182 @@ const mongoose = require('mongoose');
 const Day = require('./day.js');
 const daySchema = Day.schema;
 
+const hourSchema = new mongoose.Schema({
+  apparentTemperature: {
+    type: Number,
+  },
+  cloudCover: {
+    type: Number,
+  },
+  dewPoint: {
+    type: Number,
+  },
+  humidity: {
+    type: Number,
+  },
+  icon: {
+    type: String,
+  },
+  ozone: {
+    type: Number,
+  },
+  precipIntensity: {
+    type: Number,
+  },
+  precipProbability: {
+    type: Number,
+  },
+  pressure: {
+    type: Number,
+  },
+  summary: {
+    type: String,
+  },
+  temperature: {
+    type: Number,
+  },
+  time: {
+    type: Date,
+  },
+  visibility: {
+    type: Number,
+  },
+  windBearing: {
+    type: Number,
+  },
+  windSpeed: {
+    type: Number,
+  },
+}, {
+  timestamps: true,
+  toJSON: { virtuals: true },
+});
+
+const minuteSchema = new mongoose.Schema({
+  precipIntensity: {
+    type: Number,
+  },
+  precipProbability: {
+    type: Number,
+  },
+  time: {
+    type: Date,
+  },
+}, {
+  timestamps: true,
+  toJSON: { virtuals: true },
+});
+
+
 const querySchema = new mongoose.Schema({
   response: {
     currently: {
       apparentTemperature: {
         type: Number,
-        required: true,
       },
       cloudCover: {
         type: Number,
-        required: true,
       },
       dewPoint: {
         type: Number,
-        required: true,
       },
       humidity: {
         type: Number,
-        required: true,
       },
       icon: {
         type: String,
-        required: true,
       },
       nearestStormBearing: {
         type: Number,
-        required: true,
       },
       nearestStormDistance: {
         type: Number,
-        required: true,
       },
       ozone: {
         type: Number,
-        required: true,
       },
       precipIntensity: {
         type: Number,
-        required: true,
       },
       precipProbability: {
         type: Number,
-        required: true,
       },
       pressure: {
         type: Number,
-        required: true,
       },
       summary: {
         type: String,
-        required: true,
       },
       temperature: {
         type: Number,
-        required: true,
       },
       time: {
         type: Date,
-        required: true,
       },
       visibility: {
         type: Number,
-        required: true,
       },
       windBearing: {
         type: Number,
-        required: true,
       },
       windSpeed: {
         type: Number,
-        required: true,
       }
     },
     daily: {
       summary: {
         type: String,
-        required: true,
       },
       icon: {
         type: String,
-        required: true,
       },
       data: [daySchema],
+    },
+    flags: {
+      'darksky-stations': [String],
+      'isd-stations': [String],
+      'lamp-stations': [String],
+      'madis-stations': [String],
+      'sources': [String],
+      units: {
+        type: String,
+      },
+    },
+    hourly: {
+      summary: {
+        type: String,
+      },
+      icon: {
+        type: String,
+      },
+      data: [hourSchema],
+    },
+    latitude: {
+      type: Number,
+    },
+    longitude: {
+      type: Number,
+    },
+    minutely: {
+      data: [minuteSchema],
+      icon: {
+        type: String,
+      },
+      summary: {
+        type: String,
+      },
+    },
+    offset: {
+      type: Number,
+    },
+    timezone: {
+      type: String,
     },
   },
   _owner: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'User',
-    required: true,
+
   },
 }, {
   timestamps: true,
